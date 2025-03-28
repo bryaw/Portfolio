6645b90fd4081e4879e77c0ad85d9e65a3194b5d
bryaw <tomeniobryan184@gmail.com>
2025-03-28 14:00:03 +0800

    fix: navlink text

--- portfolio/src/app/components/NavigationLink.jsx
+++ portfolio/src/app/components/NavigationLink.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
 const NavigationLink = ({href, title, isSelected}) => {
-    const buttonStyles = isSelected ? " text-blue border-blue bg-blue text-white" : " text-blue border-textslate hover:border-blue"
+    const buttonStyles = isSelected ? " text-hoverblue border-blue bg-blue text-white" : " text-blue border-textslate hover:border-blue"
   return (   
-        <Link href={href} className={`${buttonStyles} block px-3 py-2 pl-3 pr-4 text-blue sm:text-md text-sm font-bold rounded md:p-0 hover:text-hoverblue`}>
+        <Link href={href} className={`${buttonStyles} block px-3 py-2 pl-3 pr-4 text-textslate sm:text-md text-sm font-bold rounded md:p-0 hover:text-hoverblue`}>
             {title}
         </Link>
     )   
